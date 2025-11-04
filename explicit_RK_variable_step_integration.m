639056507ba7506cf5432bf089771181355527b7
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-11-03 23:30:54 -0500

    FINISHED THANK THE GODS

--- explicit_RK_variable_step_integration.m
+++ explicit_RK_variable_step_integration.m
@@ -16,7 +16,7 @@
 %X_list: the vector of X, [X0';X1';X2';...;(X_end)'] at each time step
 %h_avg: the average step size
 %num_evals: total number of calls made to rate_func_in during the integration
-function [t_list,X_list,h_avg, num_evals, num_failed_steps] = explicit_RK_variable_step_integration(rate_func_in,tspan,X0,h_ref,BT_struct,p,error_desired)
+function [t_list,X_list,h_avg, num_evals, step_failure_rate] = explicit_RK_variable_step_integration(rate_func_in,tspan,X0,h_ref,BT_struct,p,error_desired)
     t0 = tspan(1);
     tf = tspan(2);
 
@@ -29,36 +29,38 @@ function [t_list,X_list,h_avg, num_evals, num_failed_steps] = explicit_RK_variab
     XB = X0;
     
     num_total_evals = 0;
-    num_failed_steps = 0;
+    num_fails = 0;
 
 
     while t < tf
         if t + h > tf
             h = tf - t; % land exactly on tf
         end
-
-        %Take step
-        [XB, num_evals, h_next, redo] = explicit_RK_variable_step(rate_func_in,t,XB,h,BT_struct,p,error_desired);
-        
-        num_total_evals = num_total_evals + num_evals;
-        
-
-        
-        if redo 
-            num_failed_steps = num_failed_steps + 1;
-        else % we go
-            t = t + h;
-            
-            t_list(end+1) = t;
-            X_list(end+1, :) = XB';
-            
+    
+        % --- retry logic for failed steps ---
+        redo = true;
+        while redo
+            [XB, num_evals, h_next, redo] = explicit_RK_variable_step(rate_func_in, t, XB, h, BT_struct, p, error_desired);
+            num_total_evals = num_total_evals + num_evals;
+    
+            if redo
+                num_fails = num_fails + 1;
+                h = h_next;  % reduce step and retry same t
+            else
+                t = t + h;
+                t_list(end+1) = t;
+                X_list(end+1, :) = XB';
+                h = h_next;
+            end
         end
-
-        h = h_next;
-
     end
+
+
 
     h_avg = mean(diff(t_list));
     num_evals = num_total_evals;
+
+    num_accepted_steps = length(t_list) - 1;
+    step_failure_rate = num_fails / (num_accepted_steps + num_fails);
 
 end
